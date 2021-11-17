1f8c092c32d7adf815fb386b6d3915de796e7fa8
Stalin Andres Vargas Espinosa <stalin.vargas@epn.edu.ec>
2021-11-17 08:57:05 -0500

    update/variables parte 1

---
+++ 01-javascrypt/01-variables.js
@@ -0,0 +1,41 @@
+// 01- javascript /
+//    / 01-variables.js
+
+// variables muteables e inmutables
+
+var numerouno = 1;
+let numerodos = 2;
+umerouno =5;
+numerodos = false;
+
+//variables inmutables
+const congiguracion =  "PDF";
+// siempre vamos a usar CONST > LET  > NUNCA VAR
+
+// tipos de variables
+const numero = 1;
+const sueldo = 1.2;
+const texto =  "Adrian";
+const boolean = false;
+const zapatos = undifined;
+// imprimir el tipo de variable
+console.log(typeof zapatos);
+console.log(typeof texto);
+
+
+if(""){
+    console.log( "String vacio es verdadero" );
+}else{
+    console.log( "String vacio es Falsy" );
+}
+
+// objetos json y arreglos
+const arreglo = {
+    nombre=   "Adrian" ,
+    edad =32,
+    casado = false,
+    ropa:{
+        color:'plomo', talla: '40'
+    },
+    mascotas['cachetes','pequitas','panda']
+};
